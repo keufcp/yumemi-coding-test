2fadddd1c9d15936d09b0a2e2a288260efdfd9df
keufcp <149170099+keufcp@users.noreply.github.com>
2025-03-09 15:17:45 +0900

    Addition and editing of preferences, etc...

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,7 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        customOlive: "#535c13", // カスタムカラー
       },
     },
   },
